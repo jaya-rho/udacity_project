0755d206da3046d8c1b4b815bb6fdc7af2763b4e
jaya-rho <hidemotionn@gmail.com>
2021-01-18 00:25:50 +0900

    enabled to run inference and post-process of DNN

--- nd131-openvino-fundamentals-project-starter/inference.py
+++ nd131-openvino-fundamentals-project-starter/inference.py
@@ -36,32 +36,87 @@ class Network:
 
     def __init__(self):
         ### TODO: Initialize any class variables desired ###
+        self.infer_engine_ = None
+        self.network_ = None
+        self.exec_network_ = None
+        self.input_blob_ = []
+        self.output_blob_ = []
 
-    def load_model(self):
+
+    def load_model(self, xml_model, device='CPU', cpu_ext=None):
         ### TODO: Load the model ###
-        ### TODO: Check for supported layers ###
+        print('* load model *')
+        model_dir_path = os.path.splitext(xml_model)[0]
+        bin_model = model_dir_path + '.bin'
+        print(' - xml model: %s' % (xml_model))
+        print(' - bin model: %s' % (bin_model))
+
+        # Initialize the Inference Engine
+        self.infer_engine_ = IECore()
+
         ### TODO: Add any necessary extensions ###
+        if cpu_ext and "CPU" in device:
+            self.infer_engine_.add_extension(cpu_ext, device)
+
+        # Read the IR as a IENetwork
+        self.network_ = self.infer_engine_.read_network(model=xml_model, weights=bin_model)
+
+        ### TODO: Check for supported layers ###
+        layers_map = self.infer_engine_.query_network(self.network_, "CPU")
+
+        print('* layers info *')
+        layer_num = 0
+        unsupport_layers = []
+        for layer, hw in layers_map.items():
+            if not hw in ['CPU']:
+#                print(' [U] #%d: %s [%s]' % (layer_num, layer, hw))
+                unsupport_layers.append(layer)
+            else:
+#                print(' [S] #%d: %s [%s]' % (layer_num, layer, hw))
+                pass
+            layer_num += 1
+
+        print(' - unsupported layers: %s' % unsupport_layers)
+
         ### TODO: Return the loaded inference plugin ###
+        self.exec_network_ = self.infer_engine_.load_network(self.network_, device)
+
         ### Note: You may need to update the function parameters. ###
+        self.input_blob_ = next(iter(self.network_.inputs))
+        self.output_blob_ = next(iter(self.network_.outputs))
+
+        print('* blob info *')
+        print(' - input : %s' % self.input_blob_)
+        print(' - output: %s' % self.output_blob_)
+
         return
 
     def get_input_shape(self):
         ### TODO: Return the shape of the input layer ###
-        return
+        return self.network_.inputs[self.input_blob_].shape
 
-    def exec_net(self):
+    def exec_net(self, image):
         ### TODO: Start an asynchronous request ###
+        self.exec_network_.start_async(request_id=0,inputs={self.input_blob_: image})
+
         ### TODO: Return any necessary information ###
         ### Note: You may need to update the function parameters. ###
         return
 
     def wait(self):
         ### TODO: Wait for the request to be complete. ###
+        infer_status = self.exec_network_.requests[0].wait(-1)
         ### TODO: Return any necessary information ###
         ### Note: You may need to update the function parameters. ###
-        return
+        return infer_status
 
     def get_output(self):
         ### TODO: Extract and return the output results
+        infer_output = self.exec_network_.requests[0].outputs[self.output_blob_]
+        print('  - extracting DNN output from blob (%s)' % self.output_blob_)
+        # DNN output  [1, 1, N, 7], [image_id, label, conf, x_min, y_min, x_max, y_max]
+        print('  - output shape: {}'.format(infer_output.shape))
+
         ### Note: You may need to update the function parameters. ###
-        return
+        return infer_output
+
--- nd131-openvino-fundamentals-project-starter/main.py
+++ nd131-openvino-fundamentals-project-starter/main.py
@@ -26,12 +26,14 @@ import time
 import socket
 import json
 import cv2
+import time
 
 import logging as log
 import paho.mqtt.client as mqtt
 
 from argparse import ArgumentParser
 from inference import Network
+from sys import platform
 
 # MQTT server environment variables
 HOSTNAME = socket.gethostname()
@@ -39,6 +41,18 @@ IPADDRESS = socket.gethostbyname(HOSTNAME)
 MQTT_HOST = IPADDRESS
 MQTT_PORT = 3001
 MQTT_KEEPALIVE_INTERVAL = 60
+
+# Get correct CPU extension
+if platform == "linux" or platform == "linux2":
+    CPU_EXTENSION = "/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so"
+    CODEC = 0x00000021
+elif platform == "darwin":
+    CPU_EXTENSION = "/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension.dylib"
+ #   CODEC = cv2.VideoWriter_fourcc('M','J','P','G')
+    CODEC = cv2.VideoWriter_fourcc('F', 'M', 'P', '4')
+else:
+    print("Unsupported OS.")
+    exit(1)
 
 
 def build_argparser():
@@ -75,6 +89,88 @@ def connect_mqtt():
     return client
 
 
+def filter_output(infer_output):
+
+    valid_obj = []
+
+    for b_i, b in enumerate(infer_output[:,:,:,:]):
+        for c_i, c in enumerate(b[:,:,:]):
+            for h_i, h in enumerate(c[:,:]):
+                obj_dict = {}
+                obj_dict['image_id'] = h[0]
+                obj_dict['label'] = h[1]
+                obj_dict['conf'] = h[2]
+                obj_dict['x_min'] = h[3]
+                obj_dict['y_min'] = h[4]
+                obj_dict['x_max'] = h[5]
+                obj_dict['y_max'] = h[6]
+
+                # only save the valid object
+                # TODO: filter by confidence
+                if not all(v == 0 for v in obj_dict.values()):
+                    valid_obj.append(obj_dict)
+
+#    print('res: {}'.format(valid_obj))
+
+    return valid_obj
+
+def draw_boundingbox(image, infer_output, image_width, image_height):
+
+    out_image = image.copy()
+    print('  - input image: [width] %d, [height] %d' % (image.shape[1], image.shape[0]))
+
+    def check_valid_range(val, max_val):
+        if val < 0:
+            val = 0
+        elif val > max_val:
+            val = max_val
+        else:
+            pass
+        return val
+
+    valid_obj_num = 0
+
+    for obj_info in infer_output:
+        conf = obj_info['conf']
+        # filter by confidence
+        if conf >= 0.7:
+            # calculate bbox coordinate
+            xmin = int(obj_info['x_min'] * image_width)
+            ymin = int(obj_info['y_min'] * image_height)
+            xmax = int(obj_info['x_max'] * image_width)
+            ymax = int(obj_info['y_max'] * image_height)
+
+            # round up into valid range
+            xmin = check_valid_range(xmin, image_width)
+            ymin = check_valid_range(ymin, image_height)
+            xmax = check_valid_range(xmax, image_width)
+            ymax = check_valid_range(ymax, image_height)
+
+            # draw bbox
+            cv2.rectangle(out_image, (xmin, ymin), (xmax, ymax), (0, 0, 255), 2)
+
+            valid_obj_num += 1
+            print('  - draw bbox [%f, %f, %f, %f] confidence: %f' % (xmin,ymin,xmax,ymax,conf))
+
+    # save resulting image
+#    cv2.imwrite('{}_output.png'.format(obj_info['image_id']), out_image)
+
+    # assert if one more people are detected per frame
+    if valid_obj_num > 1:
+        assert False, 'people counter > 1 in one frame'
+
+    return out_image, valid_obj_num
+
+
+def add_infer_time(image, infer_time_ms):
+    # add text
+    loc = (10, 20)
+    cv2.putText(image, "inference time(without post-process): %.2fms" % (infer_time_ms), loc, cv2.FONT_HERSHEY_SIMPLEX, 0.4, (209, 130, 0, 255), 1)
+    print('  - inference time: %.2fms' % (infer_time_ms))
+
+    return image
+
+
 def infer_on_stream(args, client):
     """
     Initialize the inference network, stream video to network,
@@ -84,37 +180,98 @@ def infer_on_stream(args, client):
     :param client: MQTT client
     :return: None
     """
-    # Initialise the class
+    # Initialise the classgg
     infer_network = Network()
     # Set Probability threshold for detections
     prob_threshold = args.prob_threshold
 
     ### TODO: Load the model through `infer_network` ###
+    # load_model(self, xml_model, device='CPU', cpu_ext=None)
+    infer_network.load_model(args.model, args.device, args.cpu_extension)
 
-    ### TODO: Handle the input stream ###
+    ### TODO: Handle the inpgut stream ###
+    # Get and open video capture
+    # TODO: support for images
+    cap = cv2.VideoCapture(args.input)
+    cap.open(args.input)
+    # [1, 3, 320, 544] (BCHW)
+    net_input_dims = infer_network.get_input_shape()
+    print('* DNN input dims: {}'.format(net_input_dims))
+
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+    # * Video dims: [height:432, width:768]
+    print('* Video dims: [height:{}, width:{}]'.format(height, width))
+
+    print('platform: {}'.format(platform))
+    out_video = cv2.VideoWriter('out_result.mp4', CODEC, 30, (width, height))
 
     ### TODO: Loop until stream is over ###
+    frame_num = 0
 
+    while cap.isOpened():
         ### TODO: Read from the video capture ###
+        # Read the next frame
+        flag, frame = cap.read()
+        if not flag:
+            break
+        key_pressed = cv2.waitKey(60)
 
-        ### TODO: Pre-process the image as needed ###
+        ### TODO: Pre-processg the image as needed ###
+        # cv2.resize(src, dsize=(width, height))
+        p_frame = cv2.resize(frame, (net_input_dims[3], net_input_dims[2]))
+        p_frame = p_frame.transpose((2,0,1))
+        # reshape (3, 320, 544) to (1, 3, 320, 544)
+        p_frame = p_frame.reshape(1, *p_frame.shape)
+        print('+ frame %d' % (frame_num))
+        print('  - shape: {}'.format(p_frame.shape))
 
         ### TODO: Start asynchronous inference for specified request ###
+        infer_start = time.time()
+        infer_network.exec_net(p_frame)
 
         ### TODO: Wait for the result ###
+        if infer_network.wait() == 0: # when the inference per frame finishes
+            infer_stop = time.time()
+            infer_time_ms = (infer_stop-infer_start)*1e3
 
             ### TODO: Get the results of the inference request ###
+            infer_result = infer_network.get_output()
 
             ### TODO: Extract any desired stats from the results ###
+            valid_object = filter_output(infer_result)
+
+            # draw bounding box of detected person on the image
+            out_frame, person_num = draw_boundingbox(frame, valid_object, width, height)
+
+            # add inference time on the image
+            add_infer_time(out_frame, infer_time_ms)
+            # write into a movie
+            out_video.write(out_frame)
 
             ### TODO: Calculate and send relevant information on ###
             ### current_count, total_count and duration to the MQTT server ###
+            print('  - people count in current frame: %d' % (person_num))
             ### Topic "person": keys of "count" and "total" ###
             ### Topic "person/duration": key of "duration" ###
 
         ### TODO: Send the frame to the FFMPEG server ###
+#        sys.stdout.buffer.write(out_frame)
+#        sys.stdout.flush()
 
         ### TODO: Write an output image if `single_image_mode` ###
+
+        # Break if escape key pressed
+        if key_pressed == 27:
+            break
+
+        # count up the frame number
+        frame_num += 1
+
+    # Release the capture and destroy any OpenCV windows
+    cap.release()
+    out_video.release()
+    cv2.destroyAllWindows()
 
 
 def main():
@@ -125,8 +282,10 @@ def main():
     """
     # Grab command line args
     args = build_argparser().parse_args()
+
     # Connect to the MQTT server
     client = connect_mqtt()
+
     # Perform inference on the input stream
     infer_on_stream(args, client)
 
